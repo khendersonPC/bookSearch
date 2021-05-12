e53c01810be9d12ea79aff207c71148028c75db6
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-05-12 14:25:40 -0600

    added optional chaining to avoid errors-COOL

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -4,26 +4,20 @@ import BookCard from "../BookCard"
 import Col from "../Grid"
 
 export const BookList = (props)=>{
+    
     return(
         <div className= "list">
         <div class="card-deck">
             {
                 props.books.map((book, i)=>{
-                 
+
                     return <div className= "col-xs-3" >
                                 <BookCard 
                                 key={i}
-                                image= {book.volumeInfo.imageLinks.smallThumbnail}
+                                image= {book.volumeInfo.imageLinks?.smallThumbnail}
                                 title= {book.volumeInfo.title}
                                 authors= {book.volumeInfo.authors}
-                                description={{
-                                    if({book.searchInfo.textSnippet}!=Undefined){
-                                        return (book.searchInfo.textSnippet)
-                                    }
-                                    else{
-                                        return( "Undefined")
-                                    }
-                                }}
+                                description= {book.searchInfo?.textSnippet}
                                 link = {book.volumeInfo.infoLink}
                                 />
                             </div>
