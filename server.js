c11ffa240e3d424514443d94b8d4d2be50d94002
khendersonPC <20666083+khendersonPC@users.noreply.github.com>
2021-05-14 20:34:15 -0600

    added sendFile

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const path = require("path")
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/*", function(req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
+  })
 }
 // Add routes, both API and view
 app.use(routes);
